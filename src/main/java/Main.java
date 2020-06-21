75576e1084620f266d1d0d229ae4c07de6d5ea17
Inna Smirnova <moipr@yandex.ru>
2020-06-21 18:34:06 +0300

    fixed #1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,11 @@
 public class Main {
-  public static long main(String[] args) {
+  public static void main(String[] args) {
     BonusService service = new BonusService();
 
     long amount = 1000_60;
     boolean registered = true;
 
-    return service.calculate(amount, registered);
+    service.calculate(amount, registered);
   }
 }
 
